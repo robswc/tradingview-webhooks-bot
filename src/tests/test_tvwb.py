299d5bc45794bad7dde960452fca99953e4ac772
robwsc <robert.s.w.carroll@gmail.com>
2023-05-08 10:40:00 -0400

    Upgrade dependencies

--- src/tests/test_tvwb.py
+++ src/tests/test_tvwb.py
@@ -3,26 +3,31 @@ from unittest import TestCase
 
 
 class TestCLI(TestCase):
-    # change dir before running tests
 
-    os.chdir('..')
+    def setUp(self) -> None:
+        self.prefix = 'python tvwb.py'
+        self.initial_settings = open('settings.py', 'r').read()
 
-    def test_newevent(self):
-        from tvwb import newevent
-        assert newevent(name='TestEvent')
-        self.assertRaises(ValueError, newevent, name='!')
-        self.assertRaises(ValueError, newevent, name='@')
-        self.assertRaises(ValueError, newevent, name='#')
-        self.assertRaises(ValueError, newevent, name='test event')
-        self.assertRaises(ValueError, newevent, name='test-event')
-        self.assertRaises(ValueError, newevent, name='test_event')
+    def test_create_and_register_action(self):
 
-    def test_newaction(self):
-        from tvwb import newaction
-        assert newaction(name='TestAction')
-        self.assertRaises(ValueError, newaction, name='!')
-        self.assertRaises(ValueError, newaction, name='@')
-        self.assertRaises(ValueError, newaction, name='#')
-        self.assertRaises(ValueError, newaction, name='test action')
-        self.assertRaises(ValueError, newaction, name='test-action')
-        self.assertRaises(ValueError, newaction, name='test_action')
+        # create action
+        cmd = f'{self.prefix} action:create CustomAction --no-register'
+        try:
+            os.system(cmd)
+        except Exception as e:
+            self.fail(e)
+
+        # register action
+        cmd = f'{self.prefix} action:register CustomAction'
+        try:
+            os.system(cmd)
+        except Exception as e:
+            self.fail(e)
+
+    def tearDown(self) -> None:
+        os.remove('components/actions/custom_action.py')
+
+        # restore settings.py
+        with open('settings.py', 'w') as settings_file:
+            settings_file.write(self.initial_settings)
+            settings_file.close()
